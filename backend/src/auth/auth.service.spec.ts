44f1e977330b792013df49f8dd60756250e71cc0
Thiago <priotothiago@gmail.com>
2025-04-07 08:57:21 -0300

    deploy backend

--- backend/src/auth/auth.service.spec.ts
+++ backend/src/auth/auth.service.spec.ts
@@ -1,3 +1,4 @@
+// src/auth/auth.service.spec.ts
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 
@@ -12,7 +13,17 @@ describe('AuthService', () => {
     service = module.get<AuthService>(AuthService);
   });
 
-  it('should be defined', () => {
+  it('deve estar definido', () => {
     expect(service).toBeDefined();
+  });
+
+  it('deve retornar success true para email e senha corretos', async () => {
+    const result = await service.login('admin@gmail.com', '1234');
+    expect(result).toEqual({ success: true });
+  });
+
+  it('deve retornar success false para email ou senha incorretos', async () => {
+    const result = await service.login('admin@gmail.com', 'errado');
+    expect(result).toEqual({ success: false });
   });
 });
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,14 +4,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Habilitar CORS para permitir requisições do frontend
   app.enableCors({
-    origin: 'http://localhost:4200', // Permitir apenas o frontend
+    origin: 'http://localhost:4200',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
 
-  await app.listen(3000); // Porta padrão do backend
+  await app.listen(3000);
   console.log('Backend rodando em http://localhost:3000');
 }
 void bootstrap();
